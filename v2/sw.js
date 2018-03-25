d9ac1584c306909d31e4d4b9f1440a8f6bb3a762
Morix <rade1500@gmail.com>
2018-03-25 09:11:47 +0900

    add v2

---
+++ v2/sw.js
@@ -0,0 +1,38 @@
+const expectedCaches = ['static-v2'];
+
+self.addEventListener('install', event => {
+  console.log('V2 installing…');
+
+  // cache a horse SVG into a new cache, static-v2
+  event.waitUntil(
+    caches.open('static-v2').then(cache => cache.add('/v2/in-scope/gohan.png'))
+  );
+});
+
+self.addEventListener('activate', event => {
+  // delete any caches that aren't in expectedCaches
+  // which will get rid of static-v1
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (!expectedCaches.includes(key)) {
+          console.log("delete chache: " + key);
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log('V2 now ready to handle fetches!');
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  console.log(url);
+
+  // serve the horse SVG from the cache if the request is
+  // same-origin and the path is '/dog.svg'
+  if (url.origin == location.origin && url.pathname == '/v2/in-scope/man.png') {
+    event.respondWith(caches.match('/v2/in-scope/gohan.png'));
+  }
+});
